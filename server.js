6875d11cda07ab257f7748560f03e86fa0e8aae4
Ashish Makwana <ashishn.makwana@gmail.com>
2024-12-03 18:38:48 +0530

    controllers, router and db connect

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from "express";
+import "dotenv/config";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("My MERN app");
