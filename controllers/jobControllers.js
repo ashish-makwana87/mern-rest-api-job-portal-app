a8a7b01ebefa5e494ca4e54af0158d6d4ef652b5
Ashish Makwana <ashishn.makwana@gmail.com>
2024-12-07 22:51:23 +0530

    getAllJobs and getJob controller

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -2,29 +2,37 @@ import { StatusCodes } from "http-status-codes"
 import Job from "../models/JobModel.js";
 
 export const getAllJobs = async (req, res) => {
+  const allJobs = await Job.find({})
 
-  res.status(StatusCodes.OK).json({msg: "all jobs"})
-}
+  res.status(StatusCodes.OK).json({totalJobs: allJobs.length, allJobs})
+};
 
 export const createJob = async (req, res) => {
-  const job = await Job.create(req.body);
- res.status(StatusCodes.CREATED).json({job})
-}
+ const job = await Job.create(req.body);
+ res.status(StatusCodes.CREATED).json({job});
+};
 
 export const getJob = async (req, res) => {
+  const { id } = req.params;
 
- res.status(StatusCodes.OK).json({msg: "single job"})
-}
+  const job = await Job.findById(id);
+
+  if(!job) {
+    res.status(StatusCodes.NOT_FOUND).json({msg: `No job existed with Id: ${id}`})
+  }
+
+ res.status(StatusCodes.OK).json({job})
+};
 
 
 export const updateJob = async (req, res) => {
 
  res.status(StatusCodes.OK).json({msg: "update job"})
-}
+};
 
 export const deleteJob = async (req, res) => {
 
  res.status(StatusCodes.OK).json({msg: "delete job"})
-}
+};
 
 
