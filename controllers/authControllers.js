e712ffc00d71180dce89312b2799bf551bcce926
Ashish Makwana <ashishn.makwana@gmail.com>
2024-12-11 14:33:01 +0530

    hash password

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,24 +1,28 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 
 export const registerUser = async (req, res) => {
  
- const userCount = await User.countDocuments(); 
+ const userCount = await User.countDocuments();
  
  if(userCount === 0) {
   req.body.role = 'admin'
  } else {
   req.body.role = 'user'
  }
+ 
+ const hashedPassword = await hashPassword(req.body.password);
+ req.body.password = hashedPassword; 
 
- const user = await User.create(req.body)
+ const user = await User.create(req.body);
 
- res.status(StatusCodes.CREATED).json({ user })
+ res.status(StatusCodes.CREATED).json({ msg: "User created" });
 };
 
 export const loginUser = async (req, res) => {
 
- res.status(StatusCodes.OK).json({ msg: 'login user' })
+ res.status(StatusCodes.OK).json({ msg: 'login user' });
 }; 
 
