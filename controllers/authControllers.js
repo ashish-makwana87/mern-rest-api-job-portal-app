2b39b8e6cb99bf116efa57477152c290b2411811
Ashish Makwana <ashishn.makwana@gmail.com>
2024-12-11 14:12:48 +0530

    admin user setup

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,17 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 
+
 export const registerUser = async (req, res) => {
-  
+ 
+ const userCount = await User.countDocuments(); 
+ 
+ if(userCount === 0) {
+  req.body.role = 'admin'
+ } else {
+  req.body.role = 'user'
+ }
+
  const user = await User.create(req.body)
 
  res.status(StatusCodes.CREATED).json({ user })
