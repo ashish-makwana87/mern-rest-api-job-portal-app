291237b024cee33b2e1dfe0c633e4a81e5df8340
Ashish Makwana <ashishn.makwana@gmail.com>
2024-12-23 17:45:06 +0530

    login error msg display

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,20 +12,29 @@ import { toast } from "react-toastify";
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  
+  const errObj = { msg: "" };
 
+  if (data.password.length < 6) {
+    errObj.msg = "Password must be of at least 6 characters";
+    return errObj;
+  }
+  
   try {
     await customFetch.post("/auth/login", data);
     toast.success("Welcome");
     return redirect("/dashboard");
   } catch (error) {
-    return error?.response?.data?.msg;
+    errObj.msg = error?.response?.data?.msg;
+    return errObj;
   }
 };
 
 function Login() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
-  const actionData = useActionData();
+  
+  const errObj = useActionData();
 
   return (
     <section className='alignment my-20'>
@@ -34,9 +43,7 @@ function Login() {
         className='flex flex-col gap-y-4 shadow-2xl p-8 justify-center mx-auto md:w-96'
       >
         <h2 className='text-center mb-4'>Login</h2>
-        {actionData && (
-          <p className='text-center text-red-600 mb-2'>{actionData}</p>
-        )}
+        {errObj?.msg && <p className="text-center text-red-600 mb-2">{errObj.msg}</p> }
         <FormRow type='email' name='email' labelText='your email' />
         <FormRow type='password' name='password' labelText='password' />
         <button type='submit' className='btn mt-2'>
